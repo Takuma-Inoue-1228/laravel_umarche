0c922e1a05838b531d34b09a6c7fbd4e1a1cb410
TakumaInoue <inouetakuma19921228@gmail.com>
2021-08-16 19:45:38 +0900

    atrribute_bag

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,6 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー1</x-slot>
   コンポーネントテスト1
-  <x-tests.card title="タイトル1" content="本文" :message='$message' />
-  <x-tests.card  />
+  <x-tests.card title="タイトル1" content="本文" :message="$message" />
+  <x-tests.card title="CSSを変更したい" class="bg-red-200" />
 </x-tests.app>
