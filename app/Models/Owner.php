34ebc6400a4669c7bc6121baefdcf1af9c3208c3
TakumaInoue <inouetakuma19921228@gmail.com>
2021-08-21 17:42:59 +0900

    multi_login

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -10,7 +10,18 @@ class Owner extends Authenticatable
 {
     use HasFactory;
 
-        /**
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
