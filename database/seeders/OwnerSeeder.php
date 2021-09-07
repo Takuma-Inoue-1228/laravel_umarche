2a6da5ee239e2f06977edc5e3aad21b9d965d9b1
TakumaInoue <inouetakuma19921228@gmail.com>
2021-09-07 12:52:14 +0900

    carbon

---
+++ database/seeders/OwnerSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('owners')->insert([
+            [
+            'name' => 'test1',
+            'email' => 'test1@test.com',
+            'password' => Hash::make('19921228'),
+            'created_at' => '1992/12/28 00:00:00',],
+            [
+            'name' => 'test2',
+            'email' => 'test2@test.com',
+            'password' => Hash::make('19921228'),
+            'created_at' => '1992/12/28 00:00:00',],
+            [
+            'name' => 'test3',
+            'email' => 'test3@test.com',
+            'password' => Hash::make('19921228'),
+            'created_at' => '1992/12/28 00:00:00',]
+        ]);
+    }
+}
