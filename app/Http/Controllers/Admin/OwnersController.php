994885c492d773058ec6d7fad051991e74fc8a03
TakumaInoue <inouetakuma19921228@gmail.com>
2021-09-10 20:00:36 +0900

    owners_stpre

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -55,7 +55,9 @@ class OwnersController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        return redirect()->route('admin.owners.index');
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message', 'オーナー登録を実施しました。');
     }
 
     /**
