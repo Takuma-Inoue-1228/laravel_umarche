c4d8c6b8052eaa2d609d5062fc6f8a29ce59820d
TakumaInoue <inouetakuma19921228@gmail.com>
2021-09-16 23:00:26 +0900

    add shop

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -20,7 +20,7 @@ class OwnersController extends Controller
     public function index()
     {
         $owners = Owner::select('id', 'name', 'email', 'created_at')
-        ->paginate(3);
+        ->paginate(5);
         
         return view(
             'admin.owners.index',
